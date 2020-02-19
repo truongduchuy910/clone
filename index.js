447c01995195b5f1306e275b36fc822442e7a388
truongduchuy910 <truongduchuy910>
2020-02-19 10:54:25 +0700

    [code] clone

--- index.js
+++ index.js
@@ -16,57 +16,62 @@ var download = function(uri, filename) {
   });
 };
 mongoose
-  .connect("mongodb://localhost:27017/heroku_g4vd37n3", {
+  .connect("mongodb://139.180.214.97:27017/heroku_g4vd37n3", {
     useNewUrlParser: true
   })
   .then(() => {
     console.log("CONNECTION SUCCESS");
-  });
-var db = require("./schemas.js");
-db.product.basis.find(
-  {
-    //		name: "BPI Best Pre Workout 30SERS"
-  },
-  async (err, docs) => {
-    console.log(docs.length);
-    let p;
-    for (let i = 0; i < docs.length; i++) {
-      p = docs[i];
-      console.log(i);
+    var db = require("./schemas.js");
+    db.product.basis.find(
+      {
+        //		name: "BPI Best Pre Workout 30SERS"
+      },
+      async (err, docs) => {
+        console.log(docs.length);
+        let p;
+        for (let i = 0; i < docs.length; i++) {
+          p = docs[i];
+          console.log(p.name);
+          /*DOWNLOAD*
       await download(
         "https://shopwheydanang.com/store/" + p.images[0],
         "./p/" + i + "." + p.name + ".png"
       );
-      let information = await db.product.information.find({
-        pointId: p.id
-      });
-      let content = "";
-      let guide = "";
-      let detailImage = "";
-      information.map(info => {
-        if (info.name == "description") {
-          info.delta.forEach(action => {
-            content += action.insert;
+      /**/
+          let information = await db.product.information.find({
+            pointId: p.id
           });
-        } else if (info.name == "guide") {
-          info.delta.forEach(action => {
-            guide += action.insert;
-          });
-        } else {
-          info.delta.forEach(e => {
-            if (e && e.insert && e.insert.image) {
-              detailImage = e.insert.image;
+          let content = "";
+          let guide = "";
+          let detailImage = "";
+          information.map(info => {
+            if (info.name == "description") {
+              info.delta.forEach(action => {
+                content += action.insert;
+              });
+            } else if (info.name == "guide") {
+              info.delta.forEach(action => {
+                guide += action.insert;
+              });
+            } else {
+              info.delta.forEach(e => {
+                if (e && e.insert && e.insert.image) {
+                  detailImage = e.insert.image;
+                }
+              });
             }
           });
-        }
-      });
+
+          /*DOWNLOAD*
+      await download(
       if (detailImage) {
         await download(
           detailImage,
           "./d/" + p.name + detailImage.slice(detailImage.length - 4)
         );
       }
-      let mutation = `mutation {
+	      /**/
+          let mutation = `mutation {
           createProduct(data: { 
             name: "${p.name}", 
             price: ${p.price},
@@ -77,41 +82,13 @@ db.product.basis.find(
           }
         }
         `;
-      const res = await axios({
-        url: "http://localhost:6005/admin/api",
-        method: "post",
-        data: { query: mutation }
-      });
-      if (!res.data.updateProduct) {
+          const res = await axios({
+            url: "https://ad.loaloa.me/admin/api",
+            method: "post",
+            data: { query: mutation }
+          });
+          console.log("res: ", res.data);
+        }
       }
-
-      //   let query = `query {
-      // 	allProducts(where: { name:"${p.name}" }) {
-      // 	name,id
-      // 	}
-      // }`
-      //     .replace(/ "/g, '"')
-      //     .replace(/" /g, '"');
-      //   console.log(query);
-      //   let { data } = await axios({
-      //     url: "http://localhost:6005/admin/api",
-      //     method: "post",
-      //     data: { query: query }
-      //   });
-      //   if (data.data.allProducts) {
-      //     console.log(data.data.allProducts[0]);
-      //   }
-
-      //   const { id } = await data.data.allProducts[0];
-      //   if (id) {
-
-      // 	updateProduct(
-      // 		id: "${id}",
-      // 		data: { description: """${content}""", guide: """${guide}""" }
-      // 	) {
-      // 		name
-      // 	}
-      // }`;
-    }
-  }
-);
+    );
+  });
